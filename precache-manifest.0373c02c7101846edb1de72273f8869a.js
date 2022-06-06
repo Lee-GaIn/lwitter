645717115bf1e1de69e3e1fa3429205a40695b63
Lee Gain <adore0126@gmail.com>
2022-06-06 16:08:33 +0900

    Updates

---
+++ precache-manifest.0373c02c7101846edb1de72273f8869a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3126df1033f32c65f82d",
+    "url": "/lwitter/static/css/main.a1cdc0ff.chunk.css"
+  },
+  {
+    "revision": "3126df1033f32c65f82d",
+    "url": "/lwitter/static/js/main.3126df10.chunk.js"
+  },
+  {
+    "revision": "bc8c612eb69ae35a9295",
+    "url": "/lwitter/static/js/1.bc8c612e.chunk.js"
+  },
+  {
+    "revision": "97f8996ebec154f22516",
+    "url": "/lwitter/static/js/runtime~main.97f8996e.js"
+  },
+  {
+    "revision": "0ee7ce128c01df22f632db344644cd88",
+    "url": "/lwitter/index.html"
+  }
+];
